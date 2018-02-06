408a8c43c011bb26d5de68c783e5540a9fdae930
Jeremy Lincoln <bossco28@gmail.com>
2018-02-06 06:32:28 -0500

    Add in navbar, footer, some styling

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
     <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quaerat, quo mollitia quisquam velit officia ab dolore nihil facere.</p>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <h1>Home</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, similique quam. Aliquam in vero minus earum ducimus. Repudiandae aut beatae minus, ex magnam magni neque fugit, quas omnis quisquam quos.</p>
 @endsection
 
 @section('sidebar')
